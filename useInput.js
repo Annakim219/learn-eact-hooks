8a2a54fd9aa9c656d39fa0b6cfcb7006293393cd
Annakim219 <97003692+Annakim219@users.noreply.github.com>
2022-09-08 13:47:25 +0900

    Update useInput.js

--- useInput.js
+++ useInput.js
@@ -5,16 +5,27 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
-const useInput = (initialValue) => {
+const useInput = (initialValue, validator) => {
   const [value, setValue] = useState(initialValue);
   const onChange = (event) => {
-    console.log(event.target);
+    const {
+      target: { value }
+    } = event;
+    let willUpdate = true;
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
+    }
+    if (willUpdate) {
+      setValue(value);
+    }
   };
   return { value, onChange };
 };
 
 const App = () => {
-  const name = useInput("Mr."); // 초기값 설정
+  // const maxLen = (value) => value.length <= 10; // 10글자가 넘으면 input value가 업데이트 되지 않음.
+  const maxLen = (value) => !value.includes("@"); // @를 포함하고 있으면 업데이트 되지 않음.
+  const name = useInput("Mr. ", maxLen); // 초기값 설정
   return (
     <div className="App">
       <h1>hello</h1>
